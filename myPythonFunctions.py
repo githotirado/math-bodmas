512e0443b74bd31ecb65fb527a6e49defcf6a81f
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2022-03-17 10:03:37 -0700

    try/except added and tested

--- myPythonFunctions.py
+++ myPythonFunctions.py
@@ -6,15 +6,21 @@ import pathlib
 scorestxt = pathlib.Path('./math-bodmas/userScores.txt')
 
 def getUserPoint(userName):
-    with open(scorestxt) as scoresFile:
-        # print(f"Opened {scorestxt}.")
-        for line in scoresFile:
-            content = line.strip('\n').split(',')
-            # print(f"File '{content[0]}' vs Entry '{userName}'")
-            if userName == content[0]:
-                print(f"{content[0]} {content[1]}")
-                scoresFile.close()
-                return 0
+    try:
+        with open(scorestxt) as scoresFile:
+            # print(f"Opened {scorestxt}.")
+            for line in scoresFile:
+                content = line.strip('\n').split(',')
+                # print(f"File '{content[0]}' vs Entry '{userName}'")
+                if userName == content[0]:
+                    print(f"{content[0]} {content[1]}")
+                    scoresFile.close()
+                    return 0
+    except IOError:
+        with open(scorestxt, "w") as scoresFile:
+            print(f"File {scorestxt} does not exist.  Creating it.")
+                  
+    scoresFile.close()
     return -1
     
 # Main section
